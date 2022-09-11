299fba344d758df151282e88ac463de7a17b5038
João Paulo Mazzo <mazzojp@gmail.com>
2022-09-10 21:46:43 -0300

    altera referencias

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArraysReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArraysReferencias.java
@@ -1,20 +1,25 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 
 public class TesteArraysReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Conta[] contas = new Conta[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(11, 11);
-		ContaCorrente cc2 = new ContaCorrente(22, 22);
-		
 		contas[0] = cc1;
+		
+		ContaCorrente cc2 = new ContaCorrente(22, 22);
 		contas[1] = cc2;
 		
-		System.out.println(contas[0].getNumero());
+		System.out.println(contas[1].getNumero());
+		
+		Conta ref = contas[0];
+		System.out.println(cc2.getNumero());
+		System.out.println(ref.getNumero());
 		
 	}
 	
